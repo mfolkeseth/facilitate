98955454119305b3dbdf32764bb2e32ac84f2a8b
magska <magnus.skaalsveen@netlight.com>
2015-05-16 18:22:43 +0200

    added basic server with task model

---
+++ server/facilitate-server/app/models/task.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// models/task
+
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var TaskSchema = new Schema({
+	title: String,
+	description: String
+});
+
+module.exports = mongoose.model('Task', TaskSchema);
---
+++ server/facilitate-server/server.js
@@ -0,0 +1,116 @@
+// server.js
+
+// BASE SETUP
+// =============================================================================
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://facilitate:f4cilit4te@ds031922.mongolab.com:31922/facilitate');
+
+var Task = require('./app/models/task');
+
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;        // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+// more routes for our API will happen here
+
+
+router.route('/tasks')
+
+    // create a bear (accessed at POST http://localhost:8080/api/bears)
+    .post(function(req, res) {
+        
+        var task = new Task();
+        task.title = req.body.title;
+		task.description = req.body.description;
+
+        // save the bear and check for errors
+        task.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Task created!' });
+        });
+        
+    })
+	.get(function(req, res) {
+        Task.find(function(err, tasks) {
+            if (err)
+                res.send(err);
+
+            res.json(tasks);
+        });
+    });
+	
+router.route('/tasks/:task_id')
+
+    .get(function(req, res) {
+        Task.findById(req.params.task_id, function(err, task) {
+            if (err)
+                res.send(err);
+            res.json(task);
+        });
+    })
+	
+	.put(function(req, res) {
+
+        // use our bear model to find the bear we want
+        Task.findById(req.params.task_id, function(err, task) {
+
+            if (err)
+                res.send(err);
+			
+			if (typeof req.body.title !== 'undefined') {
+				task.title = req.body.title;
+			}
+            if (typeof req.body.description !== 'undefined') {
+				task.description = req.body.description;	
+			}
+
+            // save the bear
+            task.save(function(err) {
+                if (err)
+                    res.send(err);
+
+                Task.findById(req.params.task_id, function(err, task) {
+		            if (err)
+		                res.send(err);
+		            res.json(task);
+		        });
+            });
+        });
+    });
+
+
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
