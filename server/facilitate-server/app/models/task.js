96aa560eb88bd9b444f94d15495f0fb0e075ff14
magska <magnus.skaalsveen@netlight.com>
2015-05-18 22:11:48 +0200

    Added more properties to task document

--- server/facilitate-server/app/models/task.js
+++ server/facilitate-server/app/models/task.js
@@ -8,6 +8,13 @@ var Schema       = mongoose.Schema;
 var TaskSchema = new Schema({
 	title: String,
 	description: String,
+	attachements: Array,
+	status: String,
+	assignedUser: String,
+	subscribers: Array,
+	location: String,
+	company: String,
+	comments: Array,
 });
 
 module.exports = mongoose.model('Task', TaskSchema);
